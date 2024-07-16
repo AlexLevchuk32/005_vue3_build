47cf085c98ba4aa43f7dd58d5f06e6410f523155
Alex Levchuk <alexlevchuk@mail.ru>
2024-07-16 22:02:06 +0300

    add card ui

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,37 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
 	<h1>Hello Home</h1>
+	<p>Edit me</p>
+	<div class="card__wrapper" v-for="item in items" :key="item.id">
+		<Card :name="`${item.lvl} lvl`" :title="item.title" />
+	</div>
 </template>
+
+<script>
+	import Card from '@/components/UI/Card.vue';
+
+	export default {
+		components: {
+			Card,
+		},
+
+		data() {
+			return {
+				items: [
+					{
+						id: 1,
+						title: 'Archer',
+						descr: 'Archer Archer Archer Archer',
+						lvl: 4,
+					},
+					{
+						id: 2,
+						title: 'Wizard',
+						descr: 'Wizard Wizard Wizard Wizard',
+						lvl: 2,
+					},
+				],
+			};
+		},
+	};
+</script>
